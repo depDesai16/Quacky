bbbf0795a71371577d258adc95b6fd4a4fe8c910
jakehebert201 <hebertj@etsu.edu>
2026-01-22 12:37:36 -0500

    program

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import os
-
-print ("Deleting all files UwU")
+import time
+print("Starting delete subroutine!")
+for i in range(1000):
+    time.sleep(0.1)
+    if i == 500:
+        print("50% complete!")
+    print(i)
+print("Deletions complete, thank you for your patience.")
 exit
 
