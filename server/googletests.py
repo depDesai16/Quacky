bcfc22c89623e261af271240b8b799b8cb486f97
Jakehebert201 <hebertj@etsu.edu>
2026-02-02 16:43:21 -0500

    user chat

--- server/googletests.py
+++ server/googletests.py
@@ -4,20 +4,19 @@ import os
 
 load_dotenv()
 
-
-def get_response_stream(user_input):
-    for chunk in client.models.generate_content_stream(
-        model="gemini-2.5-flash",
-        contents=user_input
-    ):
-        print(chunk.text, end="", flush=True)
-    print() 
-
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 print("Google API key loaded")
+
+chat = client.chats.create(model="gemini-2.5-flash")
+
+def send_message_stream(user_input):
+    for chunk in chat.send_message_stream(user_input):
+        print(chunk.text, end="", flush=True)
+    print()
+
 while True:
     user_input = input("Enter your prompt: ")
-    get_response_stream(user_input)
     if user_input == "exit":
         break
+    send_message_stream(user_input)
 #Gemini will say goodbye!
